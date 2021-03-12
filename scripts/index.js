74172371da151d4e68da8cd0e1b1febf23c40bd5
Andrey Ryabykh <av.ryabykh@yandex.ru>
2021-03-12 08:55:04 +0300

    feat: добавить popup

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+const popup = document.querySelector('.popup');
+const openPopupBtn = document.querySelector('.profile__edit-button');
+const closePopupBtn = document.querySelector('.popup__close');
+const closeCover = document.querySelector('.popup__cover');
+const formElement = document.querySelector('.popup__container');
+let nameInputPopup = document.querySelector('.popup__input-name');
+let aboutInputPopup = document.querySelector('.popup__input-about');
+let nameProfile = document.querySelector('.profile__name');
+let postProfile = document.querySelector('.profile__post');
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+    nameInputPopup.value = nameProfile.textContent;
+    aboutInputPopup.value = postProfile.textContent;
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    nameProfile.textContent = nameInputPopup.value;
+    postProfile.textContent = aboutInputPopup.value;
+    popup.classList.remove('popup_opened');
+}
+
+openPopupBtn.addEventListener('click', function() {
+    openPopup();
+})
+
+closePopupBtn.addEventListener('click', function() { closePopup() });
+
+closeCover.addEventListener('click', function() { closePopup() });
+
+formElement.addEventListener('submit', formSubmitHandler);
