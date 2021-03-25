ec498f262c1f9483cc0811b797ca454590114f7f
Andrey Ryabykh <av.ryabykh@yandex.ru>
2021-03-25 14:37:40 +0300

    feat: добавить 6 карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,43 @@ openPopupBtn.addEventListener('click', openPopup);
 closePopupBtn.addEventListener('click',closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach( function({name, link}){
+  addCard(name,link,);
+});
+
+function addCard(name, link){
+  const cardTemplate = document.querySelector('#cards').content;
+  const cardList = document.querySelector('.elements-list');
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__title').textContent = name;
+  cardElement.querySelector('.element__image').src = link;
+  cardList.append(cardElement);
+}
